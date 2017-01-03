a61b614a43951bb24e8b8511993bee0e7fac0896
Ado92 <chocky92@gmail.com>
2017-01-03 17:26:21 +0100

    Sensor list create - All sensors initialized

--- app/src/main/java/com/hranicky/iv/sensorcollector/MainActivity.java
+++ app/src/main/java/com/hranicky/iv/sensorcollector/MainActivity.java
@@ -21,6 +21,35 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private SensorManager mSensorManager;
     private List<Sensor> mList;
+    //Senzory:
+    private Sensor mACCELEROMETER;
+    private Sensor mAMBIENT_TEMPERATURE;
+    private Sensor mDEVICE_PRIVATE_BASE;
+    private Sensor mGAME_ROTATION_VECTOR;
+    private Sensor mGEOMAGNETIC_ROTATION_VECTOR;
+
+    private Sensor mGRAVITY;
+    private Sensor mGYROSCOPE;
+    private Sensor mGYROSCOPE_UNCALIBRATED;
+    private Sensor mHEART_BEAT;
+    private Sensor mHEART_RATE;
+
+    private Sensor mLIGHT;
+    private Sensor mLINEAR_ACCELERATION;
+    private Sensor mMAGNETIC_FIELD;
+    private Sensor mMAGNETIC_FIELD_UNCALIBRATED;
+    private Sensor mMOTION_DETECT;
+
+    private Sensor mPOSE_6DOF;
+    private Sensor mPRESSURE;
+    private Sensor mPROXIMITY;
+    private Sensor mRELATIVE_HUMIDITY;
+    private Sensor mROTATION_VECTOR;
+
+    private Sensor mSIGNIFICANT_MOTION;
+    private Sensor mSTATIONARY_DETECT;
+    private Sensor mSTEP_COUNTER;
+    private Sensor mSTEP_DETECTOR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +63,139 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         tv1.setVisibility(View.GONE);
 
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+/*
         mList= mSensorManager.getSensorList(Sensor.TYPE_ALL);
 
         for (int i = 1; i < mList.size(); i++) {
             tv1.setVisibility(View.VISIBLE);
             tv1.append("\n" + mList.get(i).getName() + "\n" + mList.get(i).getVendor() + "\n" + mList.get(i).getVersion() + "\n\n");
         }
+*/
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
+            // Success! There's a pressure sensor.
+            mACCELEROMETER = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        }
+        else {
+            // Failure! No pressure sensor.
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE) != null){
+            // Success! There's a pressure sensor.
+            mAMBIENT_TEMPERATURE = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_DEVICE_PRIVATE_BASE) != null){
+            // Success! There's a pressure sensor.
+            mDEVICE_PRIVATE_BASE = mSensorManager.getDefaultSensor(Sensor.TYPE_DEVICE_PRIVATE_BASE);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR) != null){
+            // Success! There's a pressure sensor.
+            mGAME_ROTATION_VECTOR = mSensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR) != null){
+            // Success! There's a pressure sensor.
+            mGEOMAGNETIC_ROTATION_VECTOR = mSensorManager.getDefaultSensor(Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY) != null){
+            // Success! There's a pressure sensor.
+            mGRAVITY = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null){
+            // Success! There's a pressure sensor.
+            mGYROSCOPE = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED) != null){
+            // Success! There's a pressure sensor.
+            mGYROSCOPE_UNCALIBRATED = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_BEAT) != null){
+            // Success! There's a pressure sensor.
+            mHEART_BEAT = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_BEAT);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE) != null){
+            // Success! There's a pressure sensor.
+            mHEART_RATE = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) != null){
+            // Success! There's a pressure sensor.
+            mLIGHT = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) != null){
+            // Success! There's a pressure sensor.
+            mLINEAR_ACCELERATION = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null){
+            // Success! There's a pressure sensor.
+            mMAGNETIC_FIELD = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED) != null){
+            // Success! There's a pressure sensor.
+            mMAGNETIC_FIELD_UNCALIBRATED = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_MOTION_DETECT) != null){
+            // Success! There's a pressure sensor.
+            mMOTION_DETECT = mSensorManager.getDefaultSensor(Sensor.TYPE_MOTION_DETECT);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_POSE_6DOF) != null){
+            // Success! There's a pressure sensor.
+            mPOSE_6DOF = mSensorManager.getDefaultSensor(Sensor.TYPE_POSE_6DOF);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE) != null){
+            // Success! There's a pressure sensor.
+            mPRESSURE = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY) != null){
+            // Success! There's a pressure sensor.
+            mPROXIMITY = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY) != null){
+            // Success! There's a pressure sensor.
+            mRELATIVE_HUMIDITY = mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) != null){
+            // Success! There's a pressure sensor.
+            mROTATION_VECTOR = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_SIGNIFICANT_MOTION) != null){
+            // Success! There's a pressure sensor.
+            mSIGNIFICANT_MOTION = mSensorManager.getDefaultSensor(Sensor.TYPE_SIGNIFICANT_MOTION);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_STATIONARY_DETECT) != null){
+            // Success! There's a pressure sensor.
+            mSTATIONARY_DETECT = mSensorManager.getDefaultSensor(Sensor.TYPE_STATIONARY_DETECT);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null){
+            // Success! There's a pressure sensor.
+            mSTEP_COUNTER = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        }
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR) != null){
+            // Success! There's a pressure sensor.
+            mSTEP_DETECTOR = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+        }
+
+
     }
 
     @Override
@@ -84,13 +240,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
      */
     @Override
     public void onSensorChanged(SensorEvent event) {
+  /*
         // The light sensor returns a single value.
         // Many sensors return 3 values, one for each axis.
         float lux = event.values[0];
         // Do something with this sensor value.
         tv2.setVisibility(View.VISIBLE);
         tv2.append("\n" + lux + "\n\n");
-
+*/
     }
 
     /**
@@ -113,10 +270,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onResume() {
         super.onResume();
 
-
+/*
         for (int i = 1; i < mList.size(); i++) {
             mSensorManager.registerListener(this, mList.get(i), SensorManager.SENSOR_DELAY_NORMAL);
         }
+ */
+
     }
 
     @Override
